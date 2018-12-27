a69ad0390d8d935a5567584d0bb989d0ea018ec6
Andrei Otea <otea@optymyze.com>
2018-12-27 10:22:19 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const App = () => {
+	return (
+		<div className="app">
+			Hello world!
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
